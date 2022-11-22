e98634c0434a8f9f713b0b6036c6674862c2918a
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2022-11-22 11:18:47 -0700

    Updates

--- de-news/components/NormalArticle.jsx
+++ de-news/components/NormalArticle.jsx
@@ -10,10 +10,10 @@ import Link from 'next/link';
 const Article = (props) => {
 
     return (
-        <div className='mt-5 mb-5'>
-            <div className={'card p-5 w-screen max-w-3xl bg-base-200 shadow-xl hover:border'}>
+        <div className='m-10'>
+            <div className={'card p-5 max-w-max whitespace-pre-wrap bg-base-200 shadow-xl hover:border'}>
                 <Link href={`/article/${props.id}`}>
-                    <div id='text' className='card-body'>{parse(props.text)}</div>
+                    <span id='text' className='card-body inline-block'>{parse(props.text)}</span>
                 </Link>
                 <div className='card-actions flex items-end'>
                     <div className='flex-1'>
